5660862e136abdd450839a2bbf5282b221a814b7
CarsonNordhoff <carsonnord@comcast.net>
2026-09-15 16:53:23 -0400

    Added logger.py for logging.
    Added logging to load_data to log data loading and data inspection.

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,12 +1,18 @@
 from utils.directories import CSV_DIR
+from utils.logger import get_logger
 import pandas as pd
+
+data_logger = get_logger(__name__)
 
 #load data from raw csvs
 def load_data():
 
     raw_data = []
 
+    data_logger.info("Loading raw data...")
     for csv_file in CSV_DIR:
+
+        data_logger.info(f"Successfully loaded {csv_file}")
         df = pd.read_csv(csv_file)
         df.columns = df.columns.str.lower()
         raw_data.append(df)
@@ -15,7 +21,8 @@ def load_data():
 
 def inspect_data(data):
     for raw_csv in data:
-        print(raw_csv.info())
-        print(raw_csv.head())
+        data_logger.info(f"Data inspection for {raw_csv}")
+        data_logger.info(raw_csv.head())
+        data_logger.info(raw_csv.info())
 
 
